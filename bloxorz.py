a15a46d0df70aa7a09c6e7d203e40d85459c156e
Viliami <viliamiuchiha@gmail.com>
2015-11-28 02:09:48 +0000

    Fixed bloxor shape

--- bloxorz.py
+++ bloxorz.py
@@ -11,28 +11,40 @@ class Block:
     def __init__(self):
         self.x = 250
         self.y = 250
-        self.width = 50
-        self.height = 50
+        self.width = 40
+        self.height = 30
         
     def draw(self):
-	    angle_length = 21.65
-	    #print(0.433*self.width)
+        x,y,w,h = self.x,self.y,self.width,self.height
+        ang = 15
+        b_w = 80
+        #pygame.draw.polygon(display,(199,208,207),[(x-10,y),(x-10+w,y-10),(x+ang+w,y-10+h),(x+ang,y+h)])
+        #pygame.draw.polygon(display,(0,0,0),[(x-10,y),(x-10+w,y-10),(x+ang+w,y-10+h),(x+ang,y+h)],1)
+        pygame.draw.line(display,(45,56,65),(x-10,y),(x-10,y-b_w))
+        pygame.draw.line(display,(45,56,65),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w))
+        pygame.draw.line(display,(45,56,65),(x+ang,y+h),(x+ang,y+h-b_w))
+        #pygame.draw.line(display,(45,56,65),(x-10+w,y-10),(x-10+w,y-10-b_w))
 
-	    pygame.draw.line(display,(0,0,0),(self.x,self.y),(self.x,self.y+self.height))
-	    #pygame.draw.line(display,(255,0,0),(self.x,self.y-self.height),(self.x,self.y))
+        #pygame.draw.line(display,(45,56,65),(x+ang,y+h+10),(x+ang+w,y+h))
+        #pygame.draw.line(display,(45,56,65),(x+ang,y+h+10),(x-10,y+10))
+        pygame.draw.polygon(display,(76,48,34),[(x-10+w,y-10),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w),(x-10+w,y-10-b_w)])
+        pygame.draw.polygon(display,(0,0,0),[(x-10+w,y-10),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w),(x-10+w,y-10-b_w)],1)
 
-	    pygame.draw.line(display,(0,0,0),(self.x+self.width,self.y-angle_length),(self.x+self.width,self.y+self.height-angle_length))
-	    pygame.draw.line(display,(0,0,0),(self.x-self.width,self.y-angle_length),(self.x-self.width,self.y+self.height-angle_length))
-	
-	    pygame.draw.line(display,(0,0,255),(self.x,self.y),(self.x-self.width,self.y-angle_length))
-	    pygame.draw.line(display,(0,0,255),(self.x,self.y+self.height),(self.x-self.width,self.y+self.height-angle_length))
+        pygame.draw.polygon(display,(76,48,34),[(x-10+w,y-10),(x-10,y),(x-10,y-b_w),(x-10+w,y-10-b_w)])
+        pygame.draw.polygon(display,(0,0,0),[(x-10+w,y-10),(x-10,y),(x-10,y-b_w),(x-10+w,y-10-b_w)],1)
 
-	    pygame.draw.line(display,(0,0,255),(self.x,self.y),(self.x+self.width,self.y-angle_length))
-	    pygame.draw.line(display,(0,0,255),(self.x,self.y+self.height),(self.x+self.width,self.y+self.height-angle_length))
+        pygame.draw.polygon(display,(76,48,34),[(x-10,y),(x+ang,y+h),(x+ang,y+h-b_w),(x-10,y-b_w)])
+        pygame.draw.polygon(display,(0,0,0),[(x-10,y),(x+ang,y+h),(x+ang,y+h-b_w),(x-10,y-b_w)],1)
 
-	    pygame.draw.line(display,(0,0,0),(self.x,self.y-self.height),(self.x+self.width,self.y-angle_length))
-	    pygame.draw.line(display,(0,0,0),(self.x,self.y-self.height),(self.x-self.width,self.y-angle_length))
+        pygame.draw.polygon(display,(76,48,34),[(x+ang+w,y-10+h),(x+ang,y+h),(x+ang,y+h-b_w),(x+ang+w,y-10+h-b_w)])
+        pygame.draw.polygon(display,(0,0,0),[(x+ang+w,y-10+h),(x+ang,y+h),(x+ang,y+h-b_w),(x+ang+w,y-10+h-b_w)],1)
 
+    def rotate_around(self,degrees,point):
+        pX,pY = point
+        xRot = pX + (math.cos(degrees)*(x-pX))-(math.sin(degrees)*(y-pY))
+        yRot = pY + (math.sin(degrees)*(x-pX))-(math.cos(degrees)*(y-pY))
+
+        
 
 class Tile:
     def __init__(self,x=100,y=100):
@@ -61,6 +73,8 @@ class Tile:
 bloxor = Block()
 block2 = Block()
 block2.y -= block2.height
+bloxor.x = 165
+bloxor.y = 220
 
 new_tile = Tile(100,100)
 new_tile_2 = Tile(140,100-10)
@@ -135,11 +149,12 @@ while(True):
     handle_events()
     display.fill((193,39,1)) #fill screen white
     
-    bloxor.draw()
-    block2.draw()
+    
+    #block2.draw()
     #new_tile_2.draw()
     #new_tile.draw()
     draw_level(level_array)
+    bloxor.draw()
 
     pygame.display.flip() #update screen
 
